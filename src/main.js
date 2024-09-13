691edd717457a5fb1fc03d6c35d8fc0e4773d08e
Phan Duy Linh <linh.phan@tomosia.com>
2024-09-13 22:29:15 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,53 @@
+import './assets/js/jquery-3.4.1.min.js';
+import './assets/js/bootstrap.min.js';
+// import './assets/js/aos.js';
+import './assets/js/custom.js';
+// import './assets/js/daterangepicker.js';
+import './assets/js/jarallax.min.js';
+// import './assets/js/moment.min.js';
+import './assets/js/owl.carousel.min.js';
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+import './assets/css/aos.css';
+import './assets/css/style.css';
+import './assets/css/daterangepicker.css';
+import './assets/css/styleLogin.css';
+import './assets/css/jquery.fancybox.min.css';
+import './assets/css/owl.carousel.min.css';
+import './assets/fonts/icomoon/style.css';
+import './assets/fonts/flaticon/font/flaticon.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import 'waypoints/lib/jquery.waypoints';
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { Popover, Modal } from 'bootstrap'
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Khởi tạo Modal
+  const modalElement = document.getElementById('myModal');
+  if (modalElement) {
+    const modal = new Modal(modalElement);
+    // Show the modal
+    modal.show();
+  }
+
+  // Khởi tạo Popover
+  const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+  popoverTriggerList.forEach((popoverTriggerEl) => {
+    new Popover(popoverTriggerEl);
+  });
+});
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(AOS.init())
+
+app.mount('#app')
